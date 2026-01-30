aa6b80df79756f3fdd7ca3275ce82cb9a25c7e4e
val_l <contacttovalerie@gmail.com>
2026-01-29 19:14:14 -0500

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <header>
+        <div className='navigation'>
+          <button className='navigation-button'>Home</button>
+          <button className='navigation-button'>Role</button>
+        </div>
+        <div className='date'>1/29/2026</div>
+      </header>
+    
+      <div className='landing-hero'>
+        <h1>Interview Test</h1>
+        <button className='start-button'>Start</button>
+      </div>
+    
+      <footer>
+        <div>Chingu 2025</div>
+        <a href='https://github.com/chingu-voyages/V59-tier1-team-03'>Github</a>
+      </footer>
+
+    
+    </>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App
