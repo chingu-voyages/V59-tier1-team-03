e1b2d1e15f503904cea39a701c83f8b11b19c86b
val_l <contacttovalerie@gmail.com>
2026-02-04 17:06:07 -0500

    Added styling for question page

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -5,7 +5,7 @@ const Questions = () => {
     <>
       <div className="question-main">
         <div className="question-title">
-          <h2 className="question-title">Questions</h2>
+          <h2 className="question-title-h2">Questions</h2>
           <div className="question-number">1/3</div>
         </div>
 
@@ -13,32 +13,41 @@ const Questions = () => {
           <div className="display-question">A developer asks you to clarify a requirement for a user story during the Sprint. What is the best immediate action?</div>
           <div className="answer-wrapper">
             <div className="answer-bubble">
-              <input type="checkbox" />
-              <h3>A</h3>
-              <p>Tell them to wait until the next Sprint Planning meeting.</p>
+              <div className="checkbox-container">
+              </div>
+              <div className="answer-text-wrapper">
+                <h3 className='question-letter'>A</h3>
+                <p>Tell them to wait until the next Sprint Planning meeting.</p>
+              </div>
+            </div>
+
+            <div className="answer-bubble-active ">
+              <div className="checkbox-container-active">
+              </div>
+              <div className="answer-text-wrapper">
+                <h3 className='question-letter'>B</h3>
+                <p>Tell them to wait until the next Sprint Planning meeting.</p>
+              </div>
             </div>
 
             <div className="answer-bubble">
-              <input type="checkbox" />
-              <h3>A</h3>
-              <p>Tell them to wait until the next Sprint Planning meeting.</p>
+              <div className="checkbox-container">
+              </div>
+              <div className="answer-text-wrapper">
+                <h3 className='question-letter'>A</h3>
+                <p>Tell them to wait until the next Sprint Planning meeting.</p>
+              </div>
             </div>
 
             <div className="answer-bubble">
-              <input type="checkbox" />
-              <h3>A</h3>
-              <p>Tell them to wait until the next Sprint Planning meeting.</p>
-            </div>
-
-            <div className="answer-bubble">
-              <input type="checkbox" />
-              <h3>A</h3>
-              <p>Tell them to wait until the next Sprint Planning meeting.</p>
-            </div>            
+              <div className="checkbox-container">
+              </div>
+              <div className="answer-text-wrapper">
+                <h3 className='question-letter'>A</h3>
+                <p>Tell them to wait until the next Sprint Planning meeting.</p>
+              </div>
+            </div>           
           </div>
-
-
-
         </div>
 
         <div className="question-navigation">
