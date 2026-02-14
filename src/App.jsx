b9d4ca70d0c942978b5c4db9151429c1d7af0487
val_l <contacttovalerie@gmail.com>
2026-02-14 17:53:22 -0500

    Added summary page and polished role page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import {Routes, Route} from 'react-router-dom'
 import RoleSelection from "./pages/Roles";
 import Questions from "./pages/Questions";
-
+import Summary from "./pages/Summary";
 
 const App = () => {
   return (
@@ -16,6 +16,7 @@ const App = () => {
         <Route path="/roles" element={<RoleSelection/>}/>
         <Route path="/questions" element={<Questions/>}/>
         <Route path="/questions/:role" element={<Questions/>}/>
+        <Route path="/summary" element={<Summary/>}/>        
       </Routes>
 
       <Footer />
