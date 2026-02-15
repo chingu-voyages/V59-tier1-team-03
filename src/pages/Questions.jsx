8852a59aec38474e00e5df3e3770080f9445be58
Timmy <afolayant537@gmail.com>
2026-02-15 08:24:47 +0100

    Fix undefined in role picking

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Checkmark from '../components/checkmark'
-import Answerbubble from '../components/question-page/answer-bubble'
-import { useParams } from 'react-router-dom'
-import questions from '../flashcards'
+import React from "react";
+import Checkmark from "../components/checkmark";
+import Answerbubble from "../components/question-page/answer-bubble";
+import { useParams } from "react-router-dom";
+import questions from "../flashcards";
 
 const Questions = () => {
-    
-  const {role} = useParams();
+  const { role } = useParams();
 
   /* let flashcards;
   switch (role) {
@@ -31,46 +30,66 @@ const Questions = () => {
   
   flashcards = flashcards.flashcards */
 
-  const roleData = questions[role]
+  const roleData = questions[role];
   const flashcards = roleData ? roleData.flashcards : [];
   
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
 
-  const [currentQuestion, setCurrentQuestion] = React.useState(0)
+  if (!flashcards || flashcards.length === 0) {
+    return (
+      <div className="question-main">
+        <h2>No questions available for this role.</h2>
+      </div>
+    );
+  }
+  const currentCard = flashcards[currentQuestion];
 
   const nextQuestion = () => {
-    setCurrentQuestion((prev)=> prev + 1 < flashcards.length ? prev + 1 : prev)
-  }
-  
+    setCurrentQuestion((prev) =>
+      prev + 1 < flashcards.length ? prev + 1 : prev,
+    );
+  };
+
   const previousQuestion = () => {
-    setCurrentQuestion((prev)=> prev - 1 >=0 ? prev - 1 : prev)
-  }
+    setCurrentQuestion((prev) => (prev - 1 >= 0 ? prev - 1 : prev));
+  };
 
   return (
     <>
       <div className="question-main">
         <div className="question-title">
           <h2 className="question-title-h2"> {role} Questions</h2>
-          <div className="question-number highlight">{`${currentQuestion + 1 } / ${flashcards.length}`}</div>
+          <div className="question-number highlight">{`${currentQuestion + 1} / ${flashcards.length}`}</div>
         </div>
 
         <div className="question-and-answer-wrapper">
-          <div className="display-question">{flashcards[currentQuestion].question || "No question available"}</div>
+          <div className="display-question">
+            {currentCard?.question || "No question available"}
+          </div>
           <div className="answer-wrapper">
-            {flashcards[currentQuestion].options && Object.entries(flashcards[currentQuestion].options).map(([key, value]) => (
-              <Answerbubble key={key} letter={key} answer={value} selected={key === flashcardds[currentQuestion].answer} />
-            ))}
+            {currentCard?.options &&
+              Object.entries(currentCard.options).map(([key, value]) => (
+                <Answerbubble
+                  key={key}
+                  letter={key}
+                  answer={value}
+                  selected={key === currentCard.answer}
+                />
+              ))}
           </div>
         </div>
 
         <div className="question-navigation">
-          <button onClick={previousQuestion} className="previous">Previous</button>
-          <button onClick={nextQuestion} className="next">Next</button>
+          <button onClick={previousQuestion} className="previous">
+            Previous
+          </button>
+          <button onClick={nextQuestion} className="next">
+            Next
+          </button>
         </div>
-
       </div>
-      
     </>
-  )
-}
+  );
+};
 
-export default Questions
+export default Questions;
